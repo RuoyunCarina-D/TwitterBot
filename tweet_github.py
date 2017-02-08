db35e30aa18d1cb8b010643b6c59ef839d5f1c42
Ruoyun Carina Deng <carina.deng@outlook.com>
2017-02-08 03:22:20 -0500

    Update tweet_github.py

--- tweet_github.py
+++ tweet_github.py
@@ -52,3 +52,57 @@ query_list=(
     ('"what you want to search"', 'what you want to reweet', 'maybe add a image.png'),
     ('"yolo"', 'nice try!','maybe add a image.png')
 )          
+
+''' 
+I am still working on the following, sorry:
+for q in query_list:
+    query = q[0]
+
+    tweet_list = api.search(q=query, count=20, lang="en")
+
+    tweets_answered = 0
+
+    for tweet in tweet_list:
+        screen_name = tweet.user.screen_name
+
+        # ??? avoid retweets
+        if (
+            hasattr(tweet, 'retweeted_status') or
+            'RT @' in tweet.text or
+            api.me().screen_name == screen_name
+        ):
+            print "this is a retweet, let's try with the next one"
+
+        else:
+            message = ".@{username} {message}".format(
+                username=screen_name,
+                message=alot[1]
+            )
+            image_path = "media/{image_name}".format(image_name=alot[2])
+
+            # if the tweet is a duplicate an exception is raised
+            try:
+                # follow the user
+                api.create_friendship(screen_name)
+
+                api.update_with_media(
+                    filename=image_path,
+                    status=message,
+                    in_reply_to_status_id=tweet.id
+                )
+
+                tweets_answered += 1
+                print '{tweets_answered} {query}'.format(
+                    tweets_answered=tweets_answered,
+                    query=query
+                )
+
+                print message
+
+                if tweets_answered >= 2:
+                    break
+
+            except tweepy.TweepError as e:
+                print e.message[0]['code']
+                print e.args[0][0]['code']
+'''
